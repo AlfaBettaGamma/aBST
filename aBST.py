9f18f8f909473bd52aaf38c31812c2906967e210
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-10-22 12:51:10 +0300

    update FindKeyIndex

--- aBST.py
+++ aBST.py
@@ -2,7 +2,7 @@ class aBST:
 
     def __init__(self, depth):
         # правильно рассчитайте размер массива для дерева глубины depth:
-        self.tree_size = (2 ** (depth + 1)) - 1
+        self.tree_size = 2 ** (depth + 1) - 1
         self.Tree = [None] * self.tree_size # массив ключей
     
     def FindKeyIndex(self, key):
@@ -15,25 +15,17 @@ class aBST:
                 return current_index
             elif self.Tree[current_index] < key :
                 current_index = current_index * 2 + 2
-                if current_index not in range(self.tree_size):
-                    return None
             elif self.Tree[current_index] > key :
-                current_index *= 2 + 1
-                if current_index not in range(self.tree_size):
-                    return None
+                current_index = current_index * 2 + 1
         return None # не найден
     
     def AddKey(self, key):
         result_index = self.FindKeyIndex(key)
-        if result_index == 0:
-            self.Tree[0] = key
-            return 0
-        if result_index is None:
-            return -1
-        if result_index < 0:
-            self.Tree[-result_index] = key
-            return -result_index
-        if result_index >= 0:
+        if result_index is not None:
+            result_index = abs(result_index)
+            self.Tree[result_index] = key
+            return result_index
+        else:
             return -1
         # добавляем ключ в массив
         # индекс добавленного/существующего ключа или -1 если не удалось
